6a676e56e617ffef3058bd33c056641048599bf6
issusk0 <plazasjolifenzio@gmail.com>
2026-02-28 16:50:32 -0300

    i sold so hard

--- project/project/settings.py
+++ project/project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*+wr6g555ey-bj(is8iy5(!3)*%k8&tw!z=x=!#bts2l=f^8m('
+SECRET_KEY = 'HELLO WORLD'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
