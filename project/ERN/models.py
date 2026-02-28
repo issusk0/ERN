56420c96d2fdf2acc30d121deaef059c13ec6b4a
issusk0 <plazasjolifenzio@gmail.com>
2026-02-28 02:02:23 -0300

    First push

---
+++ project/ERN/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+Class build:
+    relic_1 = #text
+    relic_2 = #text
+    relic_3 = #text
+    relic_4 = #text
+    relic_5 = #text
+    relic_6 = #text
+    character = #text
+    made_by = #user
+    
